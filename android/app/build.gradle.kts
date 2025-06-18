4ab1b054b71d03364c363c5da7b85ae4bec5947d
metissh <yldz.metinq@gmail.com>
2025-06-18 11:58:56 +0300

    çalışması lazım

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -8,7 +11,8 @@ plugins {
 android {
     namespace = "com.example.deneme01"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    //ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,10 +28,15 @@ android {
         applicationId = "com.example.deneme01"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        //minSdk = flutter.minSdkVersion
+        //targetSdk = flutter.targetSdkVersion
+        //versionCode = flutter.versionCode
+        //versionName = flutter.versionName
+        applicationId = "com.example.deneme01"
+        minSdkVersion(21)
+        targetSdkVersion(33)
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
